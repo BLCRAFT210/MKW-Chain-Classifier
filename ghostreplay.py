76ad61398dc62a40e299708a3a882ffad1377dad
BLCRAFT <brianslai@gmail.com>
2023-01-20 14:14:54 -0500

    Implement ghost replay

--- ghostreplay.py
+++ ghostreplay.py
@@ -1,5 +1,6 @@
 #Replays a ghost on Dolphin
 from dolphin import event, controller, savestate, memory
+from libyaz0 import decompress
 
 def pointer_chase(address, *chase_offsets):
         val = memory.read_u32(address)
@@ -7,33 +8,80 @@ def pointer_chase(address, *chase_offsets):
             val = memory.read_u32(val + offset)
         return val+chase_offsets[-1]
 
-#load savestate
-while True:
-    await event.frameadvance()
-    if (memory.read_u32(pointer_chase(0x809BD730, 0x28)) == 0):
-        savestate.load_from_file('/home/brian/Documents/startstate.sav')
-        break
+STICK_MAP = [0, 65, 70, 80, 90, 100, 110, 128, 155, 165, 175, 185, 195, 200, 255]
 
 #load ghost
 with open('/home/brian/Downloads/01m08s7737161 Kasey.rkg', 'rb') as f:
-    ghostdata = bytearray(f.read())
-    print(ghostdata[12] & 0x08)
-    print(ghostdata[136] << 0x08)
-    print(ghostdata[137])
-    face_button_inputs = (ghostdata[136] << 0x08) | ghostdata[137]
-    print(face_button_inputs)
-    directional_inputs = (ghostdata[138] << 0x08) | ghostdata[139]
-    print(directional_inputs)
-    trick_inputs = (ghostdata[140] << 0x08) | ghostdata[141]
-    print(trick_inputs)
+    ghostdata = f.read()
+    inputdata = ghostdata[140:]
+    if (ghostdata[12] & 0x08) != 0:
+        inputdata = decompress(inputdata)
+    faceInputCount = (inputdata[0] << 0x08) | inputdata[1] #accelerate, drift, shroom
+    directionalInputCount = (inputdata[2] << 0x08) | inputdata[3] #joystick
+    trickInputCount = (inputdata[4] << 0x08) | inputdata[5] #dpad
+    
+    faceInputs, directionalInputs, trickInputs = [], [], []
+    currentByte = 8
 
-    current_byte = 8
-    endFrame = 0
+    for i in range(faceInputCount):
+        inputs = inputdata[currentByte]
+        duration = inputdata[currentByte+1]
+        accelerator = (inputs & 0x01) != 0
+        drift = (inputs & 0x02) != 0
+        item = (inputs & 0x04) != 0
+        faceInputs.extend([(accelerator, drift, item)] * duration)
+        currentByte += 2
+    
+    for i in range(directionalInputCount):
+        inputs = inputdata[currentByte]
+        duration = inputdata[currentByte+1]
+        vertical = inputs & 0x0F
+        horizontal = (inputs >> 4) & 0x0F
+        directionalInputs.extend([(horizontal, vertical)] * duration)
+        currentByte += 2
 
-'''
-#play back ghost
-while True:
-    await event.frameadvance()
-    if (memory.read_u32(pointer_chase(0x809BD730, 0x28)) == 4):
-        break
-'''
+    for i in range(trickInputCount):
+        inputs = inputdata[currentByte]
+        duration = inputdata[currentByte+1]
+        trick = (inputs & 0x70) // 16
+        fullBytePresses = inputs & 0x0F
+        trickInputs.extend([0]*fullBytePresses*256 + [trick]*duration)
+        currentByte += 2
+
+    inputList = []
+    for i in range(len(faceInputs)):
+        inputList.append({
+            'Left': trickInputs[i]==3,
+            'Right': trickInputs[i]==4,
+            'Down': trickInputs[i]==2,
+            'Up': trickInputs[i]==1,
+            'Z': False,
+            'R': False,
+            'L': faceInputs[i][2],
+            'A': faceInputs[i][0],
+            'B': faceInputs[i][1],
+            'X': False,
+            'Y': False,
+            'Start': False,
+            'StickX': STICK_MAP[directionalInputs[i][0]],
+            'StickY': STICK_MAP[directionalInputs[i][1]],
+            'CStickX': 128,
+            'CStickY': 128,
+            'TriggerLeft': 0,
+            'TriggerRight': 0,
+            'AnalogA': 0,
+            'AnalogB': 0,
+            'Connected': True
+        })
+
+    #load savestate
+    while True:
+        await event.frameadvance()
+        if (memory.read_u32(pointer_chase(0x809BD730, 0x28)) == 0):
+            savestate.load_from_file('/home/brian/Documents/startstate.sav')
+            break
+
+    #play back ghost
+    for inputs in inputList:
+        controller.set_gc_buttons(0, inputs)
+        await event.frameadvance()
